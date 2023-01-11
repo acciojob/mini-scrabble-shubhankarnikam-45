ef9a2ba4c41797dab8d78eb7db71f86126387fd6
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-01-11 17:06:36 +0530

    adding stuff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+
+
+// var x = document.getElementById("myText").value;
+// document.getElementById("demo").innerHTML = x;
+
+function handle(){
+    var inText=document.getElementById("#evaluatedText").value;
+
+document.getElementById("#letterCount").innerHTML=inText.length;
+}
